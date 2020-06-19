1b58033bd0c793da0001dd1fcb2358a34c43bfa9
adamstok <adamstokowski@outlook.com>
2020-06-19 11:06:32 +0200

    adding venv

---
+++ WAR_PYT_S_22_django_rest_api_warsztat/project/venv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/ast/PycharmProjects/CODERSLAB/django/REST-API-Django/WAR_PYT_S_22_django_rest_api_warsztat/project/venv/bin/python3.7
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
