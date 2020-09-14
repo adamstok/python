2c313fbd1c4e2699ed27dfc6a287656da25fa572
adamstok <adamstokowski@outlook.com>
2020-09-14 22:21:59 +0200

    try_it v1

--- scripting/image_converter/convert.py
+++ scripting/image_converter/convert.py
@@ -12,6 +12,15 @@ files = os.listdir(".")
 allowed = ['jpeg','png','bmp'] 
 
 
+def check_args():
+    try:
+        sys.argv[1],sys.argv[2]
+        return True
+    except:
+        print(help_msg)
+        return False
+
+
 def check_files(file):
     file_format = file.split('.')[-1].lower()
     if file_format in allowed:
@@ -22,17 +31,24 @@ def check_files(file):
 def convert_file(file,new_format):
     name = file.split('.')[0]
     new_name = f'{name}.{new_format}'
-    Image.open(file).save(new_name)
-    print(f'{file} -> {new_name}')
+    try:
+        Image.open(file).save(new_name)
+        print(f'{file} -> {new_name}')
+    except:
+        print('An error occurred')
 
 
-if sys.argv[1] == '-a' and sys.argv[2] in allowed:
-    for f in files:
-        if check_files(f):
-            convert_file(f, sys.argv[2])
-
-if sys.argv[1] in ['h','-h','--help']:
-    print(help_msg)
+if check_args() == True:
+    if sys.argv[1] == '-a' and sys.argv[2] in allowed:
+        i = 0
+        for f in files:
+            if check_files(f):
+                convert_file(f, sys.argv[2])
+                i+=1
+            if i == 0:
+                print('Nothing to do')
+    elif sys.argv[1] in ['h','-h','--help']:
+        print(help_msg)
 
 
 
