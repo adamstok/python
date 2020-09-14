6a4453d99e0961ba667dba0c3753901d72f77c13
adamstok <adamstokowski@outlook.com>
2020-09-14 20:46:42 +0200

    checkfile

--- scripting/image_converter/convert.py
+++ scripting/image_converter/convert.py
@@ -3,6 +3,17 @@ import sys
 import os
 
 help_msg =  "Allowed files conversions: JPEG, PNG, BMP <=> JPEG, PNG, BMP "
+files = os.listdir()
+
+def check_files(file):
+    allowed = ['jpeg','png','bmp'] 
+    file_format = file.split('.')[-1].lower()
+    if file_format in allowed:
+        return True
+    return False
+
+
+
 
 
 
