79f23741b63e167e33ff97ac6e7941828e219a42
adamstok <adamstokowski@outlook.com>
2020-09-15 19:22:34 +0200

    testing

--- scripting/image_converter/convert.py
+++ scripting/image_converter/convert.py
@@ -6,28 +6,42 @@ help_msg =  """
 Allowed files conversions: JPEG, PNG, BMP <=> JPEG, PNG, BMP 
 Usage:
     python convert.py -a <new_fileformat>  (convert all files in the directory to the desired format)
+    python convert.py -r <new_size> (resize image with ex: new_size = 200x300)
     python convert.py <file1> <file2> (convert file1 to file2 )
 """
 files = os.listdir(".")
 allowed = ['jpeg','png','bmp'] 
-
+arguments = ['-a','-r']
 
 def check_args():
-    try:
-        sys.argv[1],sys.argv[2]
-        return True
-    except:
+    listed = [x for x in sys.argv]
+    if len(listed)<2:
         print(help_msg)
-        return False
+    elif listed[1] in arguments or check_files(listed[1]):
+        return listed[1:]
+    elif  listed[2] in allowed or check_files(listed[2]):
+        return listed[1:]
+    else:
+        print(help_msg)
+
+
+    # try:
+    #     sys.argv[1],sys.argv[2]
+    #     return True
+    # except:
+    #     print(help_msg)
+    #     return False
 
 
 def check_files(file):
-    file_format = file.split('.')[-1].lower()
-    if file_format in allowed:
-        return True
-    return False
+    try:
+        file_format = file.split('.')[-1].lower()
+        if file_format in allowed:
+            return True
+    except:
+        return False
 
-
+   
 def convert_file(file,new_format):
     name = file.split('.')[0]
     new_name = f'{name}.{new_format}'
@@ -38,19 +52,15 @@ def convert_file(file,new_format):
         print('An error occurred')
 
 
-if check_args() == True:
-    if sys.argv[1] == '-a' and sys.argv[2] in allowed:
-        i = 0
-        for f in files:
-            if check_files(f):
-                convert_file(f, sys.argv[2])
-                i+=1
-            if i == 0:
-                print('Nothing to do')
-    elif sys.argv[1] in ['h','-h','--help']:
-        print(help_msg)
+# if check_args() == True:
+#     if sys.argv[1] == '-a' and sys.argv[2] in allowed:
+#         i = 0
+#         for f in files:
+#             if check_files(f):
+#                 convert_file(f, sys.argv[2])
+#                 i+=1
+#             if i == 0:
+#                 print('Nothing to do')
+#     elif sys.argv[1] == '-r' and sys.argv[2].split('x')[0].isdecimal() and sys.argv[2].split('x')[-1].isdecimal():
 
-
-
-
-
+print(check_args())
