4520b1fc7ab4eb7e01fd753efa6ec4ac6572ff9f
adamstok <adamstokowski@outlook.com>
2020-09-17 20:30:26 +0200

    one file

--- scripting/image_converter/convert.py
+++ scripting/image_converter/convert.py
@@ -32,7 +32,7 @@ def check_args():
         return False
     elif (listed[1] == '-ai' and listed[2] in allowed) or (listed[1] == '-ar' and type(check_newsize(listed[2])) == list):
         return listed[1:]
-    elif check_input_files(listed[1]) and check_output_files(listed[2]):
+    elif check_input_files(listed[1]) and listed[2] in allowed_output:
         return listed[1:]
     else:
         print(help_msg)
@@ -47,19 +47,11 @@ def check_input_files(file):
     except:
         return False
 
-def check_output_files(file):
-    try:
-        file_format = file.split('.')[-1].lower()
-        if file_format in allowed_output:
-            return True
-    except:
-        return False
-
    
 def convert_file(file,new_format):
     name = file.split('.')[0]
     new_name = f'{name}.{new_format}'
-    if new_name in files:
+    if new_name in os.listdir('.'):
         print(f'Not converting {new_name}. The file already exist')
         return False
     try:
@@ -69,27 +61,6 @@ def convert_file(file,new_format):
     except:
         print('An error occurred')
         return False
-
-def convert_one_to_one(file1,file2):
-    file_type = file2.split('.')[-1]
-    if file2 in files:
-        print(f'Not converting {file2}. The file already exist')
-        return False
-    try:
-        if file_type != 'pdf':
-            Image.open(file1).save(file2)
-            print(f'{file} -> {new_name}')
-            return True
-        else:
-            Image.open(file1)
-            Image.save(file2,file_type, resolution=100.0, save_all=True)
-            print(f'{file} -> {new_name}')
-            return True
-
-    except:
-        print('An error occurred')
-        return False
-
 
 
 def convert_to_pdf(file_name,files_list):
@@ -150,10 +121,10 @@ Usage:
     python convert.py -ai <new_fileformat>  (convert all files in the directory to the desired format)
     python convert.py -ap  (convert all imgages to one pdf)
     python convert.py -ar <new_size> (resize all images with ex: new_size = 200x300 or ex: 0.5 in case of ratio)
-    python convert.py -r <file> <new_size> (resize image with ex: new_size = 200x300)
-    python convert.py <file1> <file2> (convert file1 to file2 )
+    python convert.py -r <file> <new_size> (resize image with ex: new_size = 200x300, or 0.5 for the ratio)
+    python convert.py <file1> <file_format> (convert file1 to formatted file1 )
 """
-# TODO: options: -r , <file1> <file2>, one img to one pdf
+# TODO: options: -r file ratio, convert music, docs - excel
 
 allowed = ['jpeg','png','bmp'] 
 allowed_output = ['jpeg','png','bmp','pdf'] 
@@ -182,8 +153,7 @@ if type(check_args()) == list:
         convert_to_pdf(file_name,files_list)
     elif todo[0] == '-ap' and len(files) == 0:
         print('No files detected')
-    elif check_input_files(todo[0]) and check_output_files(todo[1]):
-        convert_one_to_one(todo[0], todo[1])
+    elif check_input_files(todo[0]) and todo[1] in allowed_output:
+        convert_file(todo[0],todo[1])
     # TODO: first if should be checking the len(files)
-
 
